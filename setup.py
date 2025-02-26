4733452b31ccf1434549203b3085a31924e70e72
VisuAI <155494358+VisualXAI@users.noreply.github.com>
2025-02-26 17:30:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DocuSEAR',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[],
     include_package_data=True,
